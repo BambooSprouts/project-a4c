5b8743b15ea708add6cb13d77bccb7f4c8920067
TAKENOKO <106615681+bambooNoko@users.noreply.github.com>
2023-05-04 01:14:14 +0900

    Update index.js
    Signed-off-by: TAKENOKO <106615681+bambooNoko@users.noreply.github.com>

--- behavior_pack/scripts/modules/tag/index.js
+++ behavior_pack/scripts/modules/tag/index.js
@@ -9,12 +9,24 @@ const mcLib = new World("overworld");
 export class TagStatus {
 	static isExecuting = false;
 	static taggerLimitDecrease = false;
-	static tagger = null;
+	static taggerN = null;
 	static taggerLimit = 25;
 	static maxTaggerLimit = 25;
 	static deceasedPlayer = [];
+	/**
+	 * @param {mc.Player} entity
+	 */
+	static set tagger(entity) {
+		this.taggerN = entity;
+
+		mcLib.runCommands(mcLib.dimension, "replaceitem entity @a slot.armor.head 0 air 1 0");
+		mcLib.runCommands(entity, "replaceitem entity @s slot.armor.head 0 carved_pumpkin 1 0 {\"minecraft:item_lock\":{\"mode\":\"lock_in_slot\"}}");
+	}
+	static get tagger() {
+		return this.taggerN;
+	}
 	static get survivingPlayer() {
-		return mcLib.getPlayerList().map(player => this.deceasedPlayer.includes(player) ? null : player).filter(player => player !== TagStatus.tagger).filter(Boolean);
+		return mcLib.getPlayerList().map(player => this.deceasedPlayer.includes(player) ? null : player).filter(player => player !== this.tagger).filter(Boolean);
 	}
 	static randomlyTagged() {
 		this.tagger = this.survivingPlayer[Math.floor(Math.random() * this.survivingPlayer.length)];
@@ -36,13 +48,12 @@ export function tagStart() {
 export function tagEnd() {
 	TagStatus.isExecuting = false;
 	TagStatus.taggerLimitDecrease = false;
-	TagStatus.tagger = null;
+	TagStatus.taggerN = null;
 	TagStatus.taggerLimit = 25;
 	TagStatus.maxTaggerLimit = 25;
 	TagStatus.deceasedPlayer = [];
 
-	mcLib.runCommands(mcLib.dimension, "title @a title Game Over!!");
-	mcLib.runCommands(mcLib.dimension, "inputpermission set @a movement enabled");
+	mcLib.runCommands(mcLib.dimension, "title @a title Game Over!!", "inputpermission set @a movement enabled", "replaceitem entity @a slot.armor.head 0 air 1 0");
 
 	// mcLib.getPlayerList().forEach(player => player.teleport(new mc.Vector(2, -60, 4)));
 }
