db2bab2b0824de1090103f7df417c5f8e4557fae
TAKENOKO <106615681+bambooNoko@users.noreply.github.com>
2023-05-05 13:42:32 +0900

    Update index.js
    Signed-off-by: TAKENOKO <106615681+bambooNoko@users.noreply.github.com>

--- behavior_pack/scripts/modules/tag/index.js
+++ behavior_pack/scripts/modules/tag/index.js
@@ -9,27 +9,30 @@ const mcLib = new World("overworld");
 export class TagStatus {
 	static isExecuting = false;
 	static taggerLimitDecrease = false;
-	static taggerN = null;
+	static tagger = null;
 	static taggerLimit = 25;
 	static maxTaggerLimit = 25;
 	static deceasedPlayer = [];
-	/**
-	 * @param {mc.Player} entity
-	 */
-	static set tagger(entity) {
-		this.taggerN = entity;
-
-		mcLib.runCommands(mcLib.dimension, "replaceitem entity @a slot.armor.head 0 air 1 0");
-		mcLib.runCommands(entity, "replaceitem entity @s slot.armor.head 0 carved_pumpkin 1 0 {\"minecraft:item_lock\":{\"mode\":\"lock_in_slot\"}}");
-	}
-	static get tagger() {
-		return this.taggerN;
+	static get isInvincible() {
+		return this.taggerLimit > this.maxTaggerLimit - Math.floor(this.maxTaggerLimit / 5);
 	}
 	static get survivingPlayer() {
 		return mcLib.getPlayerList().map(player => this.deceasedPlayer.includes(player) ? null : player).filter(Boolean);
 	}
+	static get escapingPlayer() {
+		return this.survivingPlayer.filter(player => player !== this.tagger);
+	}
+	/**
+	 * @param {mc.Entity} entity
+	 */
+	static setTagger(entity) {
+		this.tagger = entity;
+
+		mcLib.runCommands(mcLib.dimension, "replaceitem entity @a slot.armor.head 0 air 1 0");
+		mcLib.runCommands(entity, "replaceitem entity @s slot.armor.head 0 carved_pumpkin 1 0 {\"minecraft:item_lock\":{\"mode\":\"lock_in_slot\"}}", "inputpermission set @s movement disabled");
+	}
 	static randomlyTagged() {
-		this.tagger = this.survivingPlayer[Math.floor(Math.random() * this.survivingPlayer.length)];
+		this.setTagger(this.survivingPlayer[Math.floor(Math.random() * this.survivingPlayer.length)]);
 
 		mcLib.runCommands(mcLib.dimension, `say 鬼は${this.tagger.nameTag}です！`);
 	}
@@ -37,11 +40,10 @@ export class TagStatus {
 
 export function tagStart() {
 	TagStatus.randomlyTagged();
-	mcLib.getPlayerList().forEach(player => player.teleport(new mc.Vector(130, -60, 50), mcLib.dimension, 0, 0));
+	// mcLib.getPlayerList().forEach(player => player.teleport(new mc.Vector(130, -59, 50), mcLib.dimension, 0, 0));
 
-	mcLib.runCommands(TagStatus.tagger, "inputpermission set @s movement disabled");
 	mcLib.getPlayerList().forEach(player => player.addEffect(mc.MinecraftEffectTypes.weakness, 100, 0, false));
-	TagStatus.survivingPlayer.filter(player => player !== TagStatus.tagger).forEach(player => player.addEffect(mc.MinecraftEffectTypes.invisibility, 100, 0, false));
+	TagStatus.escapingPlayer.forEach(player => player.addEffect(mc.MinecraftEffectTypes.invisibility, 100, 0, false));
 
 	TagStatus.isExecuting = true;
 }
@@ -55,5 +57,5 @@ export function tagEnd() {
 
 	mcLib.runCommands(mcLib.dimension, "title @a title Game Over!!", "inputpermission set @a movement enabled", "replaceitem entity @a slot.armor.head 0 air 1 0");
 
-	mcLib.getPlayerList().forEach(player => player.teleport(new mc.Vector(2, -60, 4), mcLib.dimension, 0, 0));
+	// mcLib.getPlayerList().forEach(player => player.teleport(new mc.Vector(2, -60, 4), mcLib.dimension, 0, 0));
 }
