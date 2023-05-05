919bd8ed0a406523e5a6c6395268817aebd9a3c6
TAKENOKO <106615681+bambooNoko@users.noreply.github.com>
2023-05-05 14:15:12 +0900

    Update tagged.js
    Signed-off-by: TAKENOKO <106615681+bambooNoko@users.noreply.github.com>

--- behavior_pack/scripts/modules/tag/tagged.js
+++ behavior_pack/scripts/modules/tag/tagged.js
@@ -6,19 +6,19 @@ import { TagStatus, tagEnd } from './index';
 const mcLib = new World("overworld");
 
 
-mc.world.events.entityHurt.subscribe(ev => {
-	if (TagStatus.isExecuting) {
-		TagStatus.setTagger(ev.hurtEntity);
+mc.world.events.entityHit.subscribe(ev => {
+	if (TagStatus.isExecuting && ev.hitEntity) {
+		TagStatus.setTagger(ev.hitEntity);
 
-		ev.damageSource.damagingEntity.addEffect(mc.MinecraftEffectTypes.invisibility, 100, 0, false);
+		ev.entity.addEffect(mc.MinecraftEffectTypes.invisibility, TagStatus.invincibleTick, 0, false);
 	}
 	else return;
 });
 
 mc.system.runInterval(() => {
 	if (TagStatus.isExecuting) {
-		TagStatus.tagger.addEffect(mc.MinecraftEffectTypes.speed, 100, 1, false);
-		TagStatus.escapingPlayer.forEach(player => player.addEffect(mc.MinecraftEffectTypes.weakness, 100, 9, false));
+		TagStatus.tagger.addEffect(mc.MinecraftEffectTypes.speed, TagStatus.invincibleTick, 1, false);
+		TagStatus.escapingPlayer.forEach(player => player.addEffect(mc.MinecraftEffectTypes.weakness, TagStatus.invincibleTick, 9, false));
 
 		if (!TagStatus.isInvincible) mcLib.runCommands(TagStatus.tagger, "inputpermission set @s movement enabled");
 
