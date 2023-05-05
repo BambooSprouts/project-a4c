a334dbef514bc0b02cc89a5ac2370487beaf2673
TAKENOKO <106615681+bambooNoko@users.noreply.github.com>
2023-05-05 14:36:20 +0900

    Update index.js
    Signed-off-by: TAKENOKO <106615681+bambooNoko@users.noreply.github.com>

--- behavior_pack/scripts/server/index.js
+++ behavior_pack/scripts/server/index.js
@@ -1,7 +1,7 @@
 import * as mc from '@minecraft/server';
 import { World } from '../lib/minecraft';
 
-import { tagStart, tagEnd } from '../modules/tag/index';
+import { TagStatus, tagStart, tagEnd } from '../modules/tag/index';
 
 const mcLib = new World("overworld");
 
@@ -9,7 +9,7 @@ mc.system.events.beforeWatchdogTerminate.subscribe(ev => ev.cancel = true);
 
 mc.system.events.scriptEventReceive.subscribe(ev => {
 	switch (ev.id) {
-		case "tag:start": return tagStart();
+		case "tag:start": return TagStatus.isExecuting ? tagStart() : null;
 		case "tag:end": return tagEnd();
 	}
 });
