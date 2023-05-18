d2fbbc7972b6dc1a523fc693ac876d3df82c2a1b
TAKENOKO <106615681+BambooSprouts@users.noreply.github.com>
2023-05-18 16:43:42 +0900

    Update index.js
    Signed-off-by: TAKENOKO <106615681+BambooSprouts@users.noreply.github.com>

--- behavior_pack/scripts/server/index.js
+++ behavior_pack/scripts/server/index.js
@@ -1,11 +1,23 @@
 import * as mc from '@minecraft/server';
 import { World } from '../lib/minecraft';
+import * as manage from '../lib/manage';
 
 import { TagStatus, tagStart, tagEnd } from '../modules/tag/index';
 
 const mcLib = new World("overworld");
 
 mc.system.events.beforeWatchdogTerminate.subscribe(ev => ev.cancel = true);
+
+
+mc.world.events.beforeItemUse.subscribe(ev => {
+	ev.item.typeId === "minecraft:apple" ? manage.control.show(ev.source) : null;
+});
+
+mc.world.events.beforeChat.subscribe(ev => {
+	const { sender } = ev;
+
+	manage.player.find(sender).muted ? ev.cancel = true : null;
+});
 
 mc.system.events.scriptEventReceive.subscribe(ev => {
 	switch (ev.id) {
